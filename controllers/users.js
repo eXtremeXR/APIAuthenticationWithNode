4c49486de37d57461d83a165ccde75c42d7debea
Stefan <viragsf@gmail.com>
2017-07-23 02:22:02 +0200

    Part #6 - JSON Web Tokens (JWT)

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,15 @@
+const JWT = require('jsonwebtoken');
 const User = require('../models/user');
+const { JWT_SECRET } = require('../configuration');
+
+signToken = user => {
+  return JWT.sign({
+    iss: 'CodeWorkr',
+    sub: user.id,
+    iat: new Date().getTime(), // current time
+    exp: new Date().setDate(new Date().getDate() + 1) // current time + 1 day ahead
+  }, JWT_SECRET);
+}
 
 module.exports = {
   signUp: async (req, res, next) => {
@@ -14,8 +25,11 @@ module.exports = {
     const newUser = new User({ email, password });
     await newUser.save();
 
+    // Generate the token
+    const token = signToken(newUser);
+
     // Respond with token
-    res.json({ user: 'created' });
+    res.status(200).json({ token });
   },
 
   signIn: async (req, res, next) => {
