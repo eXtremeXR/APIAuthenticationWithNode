a6974c295b870c44bf8b6a896b626ba9cdfe07fa
Michael Alaev <alaev.michael@gmail.com>
2017-09-02 21:15:08 +0300

    added test setup, added first unit test for controller

--- __tests__/server/controllers/controllers.test.js
+++ __tests__/server/controllers/controllers.test.js
@@ -5,12 +5,52 @@ const sinonChai = require('sinon-chai');
 const rewire = require('rewire');
 const { expect } = chai;
 
+const User = require('../../../server/models/user');
+const userController = rewire('../../../server/controllers/users.js');
 
 chai.use(sinonChai);
 
+let sandbox = null;
 
-describe('first test', function() {
-  it('should be true', () =>{
-    expect(true).to.eql(true);
-  })
+describe('Users controller', () => {
+  let req = {
+    user: { id: 1 },
+    value: {
+      body: {
+        email: 'bob@bobber.com',
+        password: '123456'
+      }
+    }
+  };
+  let res = {
+    json: function() {
+      return this;
+    },
+    status: function() {
+      return this;
+    }
+  };
+
+  beforeEach(() => {
+    sandbox = sinon.sandbox.create();
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  describe('secret', () => {
+    it('should return resource when called', () => {
+      sandbox.spy(console, 'log');
+      sandbox.spy(res, 'json');
+
+      userController.secret(req, res).then(() => {
+        expect(console.log).to.have.been.called;
+        expect(res.json.calledWith({ secret: 'resource' })).to.be.ok;
+        expect(res.json).to.have.been.calledWith({ secret: 'resource' });
+      });
+    });
+  });
+
+
 });
