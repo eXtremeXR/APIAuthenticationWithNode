c3cfb10cafbc12ba52ae25bcbe457d492f879fe6
Michael Alaev <alaev.michael@gmail.com>
2017-10-21 17:22:59 +0300

    added export baised of env

--- server/configuration/index.js
+++ server/configuration/index.js
@@ -1,13 +1,30 @@
-module.exports = {
-  JWT_SECRET: 'codeworkrauthentication',
-  oauth: {
-    google: {
-      clientID: 'number',
-      clientSecret: 'string'
-    },
-    facebook: {
-      clientID: 'number',
-      clientSecret: 'string'
+if(process.env.NODE_ENV === 'test'){
+  module.exports = {
+    JWT_SECRET: 'codeworkrauthentication',
+    oauth: {
+      google: {
+        clientID: 'number',
+        clientSecret: 'string'
+      },
+      facebook: {
+        clientID: 'number',
+        clientSecret: 'string'
+      }
     }
-  }
-};
+  };
+}else{
+  module.exports = {
+    JWT_SECRET: '',
+    oauth: {
+      google: {
+        clientID: '',
+        clientSecret: ''
+      },
+      facebook: {
+        clientID: '',
+        clientSecret: ''
+      }
+    }
+  };
+}
+
