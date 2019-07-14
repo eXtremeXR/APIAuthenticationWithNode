335ccf1eb6a883c85e0cd0bd297f2bd396fef0cb
Gonçalo Pedro <gpedro34@gmail.com>
2019-07-14 06:14:47 +0100

    replaced bodyParser by express native bodyParser

--- server/app.js
+++ server/app.js
@@ -1,32 +1,37 @@
-const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 mongoose.Promise = global.Promise;
-if (process.env.NODE_ENV === 'test') {
-  mongoose.connect('mongodb://localhost/APIAuthenticationTEST', { useNewUrlParser: true });
+if (process.env.NODE_ENV === "test") {
+  mongoose.connect("mongodb://localhost/APIAuthenticationTEST", {
+    useNewUrlParser: true
+  });
 } else {
-  mongoose.connect('mongodb://localhost/APIAuthentication', { useNewUrlParser: true });
+  mongoose.connect("mongodb://localhost/APIAuthentication", {
+    useNewUrlParser: true
+  });
 }
 
 const app = express();
-app.use(cookieParser())
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true
-}));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true
+  })
+);
 
 // Middlewares moved morgan into if for clear tests
-if (!process.env.NODE_ENV === 'test') {
-  app.use(morgan('dev'));
+if (!process.env.NODE_ENV === "test") {
+  app.use(morgan("dev"));
 }
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 // Routes
-app.use('/users', require('./routes/users'));
+app.use("/users", require("./routes/users"));
 
 module.exports = app;
